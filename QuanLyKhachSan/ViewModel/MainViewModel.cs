b141ce9ccc687398c19a0302d23314c86c33fa9e
Hdat298 <hdat318@gmail.com>
2021-12-18 00:42:39 +0700

    Add project files.

---
+++ QuanLyKhachSan/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKhachSan.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // xử lý
+        public MainViewModel()
+        {
+            MessageBox.Show("Data");
+        }
+    }
+}
