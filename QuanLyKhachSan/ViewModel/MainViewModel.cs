5613026f7b5b60f28eaaaeedc1ebd94f515e2cd9
Hdat298 <hdat318@gmail.com>
2021-12-24 19:13:40 +0700

    Update5

--- QuanLyKhachSan/ViewModel/MainViewModel.cs
+++ QuanLyKhachSan/ViewModel/MainViewModel.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿using QuanLyKhachSan.UserControlAll;
+using QuanLyKhachSan.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyKhachSan.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        public bool IsLoad = false;
+        public ICommand HomeCommand { get; set; }
+
+
         // xử lý
         public MainViewModel()
         {
+            HomeCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                MainWindow mainWibn;
+                HomeUC ucHome = new HomeUC();
+                mainWibn
+            }
+                );
+
+
+
         }
     }
 }
