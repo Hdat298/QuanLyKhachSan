5ac61ba8bd4a5ba626c67f368cbca7f5a8e355c7
Hdat298 <hdat318@gmail.com>
2021-12-27 20:49:42 +0700

    Small update

--- QuanLyKhachSan/Model/Room.cs
+++ QuanLyKhachSan/Model/Room.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyKhachSan.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,14 @@ using System.Threading.Tasks;
 
 namespace QuanLyKhachSan.Model
 {
-    public class Room
+    public class Room : BaseViewModel
     {
         public Phong phong { get; set; }
         public LoaiPhong loaiPhong { get; set; }
         public KhachHang khachHang { get; set; }
+
+        public string MaPhong { get => MaPhong; set { MaPhong = value; OnPropertyChanged(); } }
+        public string TinhTrang { get => TinhTrang; set { TinhTrang = value; OnPropertyChanged();  } }
+        public string TenKH { get => TenKH; set { TenKH = value; OnPropertyChanged();  } }
     }
 }
